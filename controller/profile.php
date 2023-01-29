54a8066f8b8f7c2fa352c3cf585cd3524c72092e
Zotlan <bazsizolika@gmail.com>
2023-01-29 18:12:47 +0100

    Profile Picture upload
    Users can now upload a profile picture in the Profile page. Said profile picture is then stored locally.
    I put the upload into a switch because there will be a change option as well.
    It doesn't display the profile pic yet.
    but it does upload it without any issues, so I'm satisfied for now.

--- controller/profile.php
+++ controller/profile.php
@@ -1,10 +1,24 @@
 <?php
+    
+    $action = "";
 
+    $action = $_REQUEST['action'] ?? "";
 
+    
 
-
-
-
+    switch($action){
+    case 'upload':
+        $target_dir = "uploads/";
+        $target_file = $target_dir. $_SESSION['username'].".jpg";
+    
+        if (move_uploaded_file($_FILES["profilePic"]["tmp_name"], $target_file)) {
+            echo "The file ". htmlspecialchars(basename($_FILES["profilePic"]["tmp_name"])). " has been uploaded.<br>";
+        } 
+        else {
+            echo "Sorry, there was an error uploading your file.<br>";
+        }
+    break;
+    }
 
 
 
--- view/profile.php
+++ view/profile.php
@@ -3,6 +3,31 @@ include "layout/main_head.php";
 ?>
 
 
+            <form action="index.php?page=profile&action=upload" method="post" enctype="multipart/form-data">
+                Please Upload A Profile Picture:
+                <input type="file" name="profilePic" id="fileToUpload">
+                <input type="submit" value="Upload Image" name="submit">
+                <input type="hidden" name="page" value="profile">
+                <input type="hidden" name="action" value="upload">
+            </form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
