d3423119dc0ce5a0e3031ed0e3cc5cd213e7de8e
Zotlan <bazsizolika@gmail.com>
2023-02-16 15:37:07 +0100

    Sql and Dropdown
    So it seems I forgot to upload the updated sql in the last commit, sorry about that.
    But more importantly...
    
    THE DROPDOWN WORKS AS IT SHOULD
    AT LAST

--- c31bzolidb.sql
+++ c31bzolidb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 12, 2023 at 04:49 PM
+-- Generation Time: Feb 16, 2023 at 02:55 PM
 -- Server version: 10.3.36-MariaDB-0+deb10u2
 -- PHP Version: 7.4.33
 
@@ -40,9 +40,17 @@ CREATE TABLE `category` (
 
 CREATE TABLE `chat` (
   `chat_id` int(11) NOT NULL,
-  `participant_1` int(11) NOT NULL,
-  `participant_2` int(11) NOT NULL
+  `chat_name` varchar(100) NOT NULL,
+  `publicity` int(11) NOT NULL,
+  `owner_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `chat`
+--
+
+INSERT INTO `chat` (`chat_id`, `chat_name`, `publicity`, `owner_id`) VALUES
+(1, 'General', 1, 10);
 
 -- --------------------------------------------------------
 
@@ -71,6 +79,19 @@ CREATE TABLE `further_comments` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `membership`
+--
+
+CREATE TABLE `membership` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `chat_id` int(11) NOT NULL,
+  `membership_type` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `messages`
 --
 
@@ -78,7 +99,8 @@ CREATE TABLE `messages` (
   `message_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `message_content` varchar(1000) NOT NULL,
-  `message_timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `message_timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `chat_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -95,6 +117,25 @@ CREATE TABLE `post` (
   `post_image_path` varchar(100) DEFAULT NULL,
   `cat_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Publicity`
+--
+
+CREATE TABLE `Publicity` (
+  `id` int(11) NOT NULL,
+  `p_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `Publicity`
+--
+
+INSERT INTO `Publicity` (`id`, `p_name`) VALUES
+(1, 'public'),
+(2, 'private');
 
 -- --------------------------------------------------------
 
@@ -134,7 +175,11 @@ INSERT INTO `user` (`userID`, `userEmail`, `userName`, `userPassword`, `userType
 (10, 'example@email.com', 'tester', '179ad45c6ce2cb97cf1029e212046e81', 1),
 (26, 'hosszufasz69@kuki.hu', 'HeilHilter', '6652f1488aff7d45ce587ebf5d2c0efb', 1),
 (30, 'Szar@Selectusers.net', 'DrSenkihazi', '5fd913ce9a195ccd408ef63c7752db09', 1),
-(33, 'bazsizolika@gmail.com', 'Zotlan', '391095d7004733654636ffe5d68053f8', 1);
+(33, 'bazsizolika@gmail.com', 'Zotlan', '391095d7004733654636ffe5d68053f8', 3),
+(37, 'Szar@S1electusers.net', 'DrSenkihazi2', '8f3ba5fd2beac46774ceba7798b4e2c4', 1),
+(38, 'SB@F1.hu', 'SebastianVettel', '6652f1488aff7d45ce587ebf5d2c0efb', 1),
+(39, 'asd@gmail.asd', 'ad', 'ab2b0d90bfaa90b8db4763b74b9f75b4', 1),
+(40, 'jozsef.bicsak@nebet.hu', 'bicsiteszt', '85ff04a72d4d50d1924be51d94fcb1df', 1);
 
 -- --------------------------------------------------------
 
@@ -172,8 +217,9 @@ ALTER TABLE `category`
 --
 ALTER TABLE `chat`
   ADD PRIMARY KEY (`chat_id`),
-  ADD KEY `participant_1` (`participant_1`),
-  ADD KEY `participant_2` (`participant_2`);
+  ADD KEY `chat_name` (`chat_name`),
+  ADD KEY `publicity` (`publicity`),
+  ADD KEY `owner_id` (`owner_id`);
 
 --
 -- Indexes for table `comments`
@@ -190,11 +236,21 @@ ALTER TABLE `further_comments`
   ADD KEY `comment_id` (`comment_id`);
 
 --
+-- Indexes for table `membership`
+--
+ALTER TABLE `membership`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `chat_id` (`chat_id`),
+  ADD KEY `membership_type` (`membership_type`);
+
+--
 -- Indexes for table `messages`
 --
 ALTER TABLE `messages`
   ADD PRIMARY KEY (`message_id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `chat_id` (`chat_id`);
 
 --
 -- Indexes for table `post`
@@ -203,6 +259,12 @@ ALTER TABLE `post`
   ADD PRIMARY KEY (`post_id`),
   ADD KEY `poster_id` (`poster_id`),
   ADD KEY `category_id` (`cat_id`);
+
+--
+-- Indexes for table `Publicity`
+--
+ALTER TABLE `Publicity`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `reaction`
@@ -241,7 +303,7 @@ ALTER TABLE `category`
 -- AUTO_INCREMENT for table `chat`
 --
 ALTER TABLE `chat`
-  MODIFY `chat_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `chat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `comments`
@@ -256,6 +318,12 @@ ALTER TABLE `further_comments`
   MODIFY `f_comment_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `membership`
+--
+ALTER TABLE `membership`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
@@ -268,6 +336,12 @@ ALTER TABLE `post`
   MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `Publicity`
+--
+ALTER TABLE `Publicity`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `reaction`
 --
 ALTER TABLE `reaction`
@@ -277,7 +351,7 @@ ALTER TABLE `reaction`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- Constraints for dumped tables
@@ -287,8 +361,8 @@ ALTER TABLE `user`
 -- Constraints for table `chat`
 --
 ALTER TABLE `chat`
-  ADD CONSTRAINT `chat_ibfk_1` FOREIGN KEY (`participant_1`) REFERENCES `user` (`userID`),
-  ADD CONSTRAINT `chat_ibfk_2` FOREIGN KEY (`participant_2`) REFERENCES `user` (`userID`);
+  ADD CONSTRAINT `chat_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `user` (`userID`),
+  ADD CONSTRAINT `chat_ibfk_2` FOREIGN KEY (`publicity`) REFERENCES `Publicity` (`id`);
 
 --
 -- Constraints for table `comments`
@@ -303,10 +377,18 @@ ALTER TABLE `further_comments`
   ADD CONSTRAINT `further_comments_ibfk_1` FOREIGN KEY (`comment_id`) REFERENCES `comments` (`comment_id`);
 
 --
+-- Constraints for table `membership`
+--
+ALTER TABLE `membership`
+  ADD CONSTRAINT `membership_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`userID`),
+  ADD CONSTRAINT `membership_ibfk_2` FOREIGN KEY (`chat_id`) REFERENCES `chat` (`chat_id`);
+
+--
 -- Constraints for table `messages`
 --
 ALTER TABLE `messages`
-  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`userID`);
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`userID`),
+  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`chat_id`) REFERENCES `chat` (`chat_id`);
 
 --
 -- Constraints for table `post`
